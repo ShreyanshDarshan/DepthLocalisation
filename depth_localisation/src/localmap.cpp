113d29677938d30fbc4bc24d03d69d65ea1f8921
Shreyansh Darshan <shreyanshdarshan1@gmail.com>
2020-05-13 15:41:02 +0530

    Implemented Local Map Extraction

--- depth_localisation/src/localmap.cpp
+++ depth_localisation/src/localmap.cpp
@@ -12,139 +12,87 @@
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>
+#include <pcl/octree/octree_search.h>
 
-#define leaf 0.4
+
+#define resolution 2.0f
 
 using namespace std;
 
-ofstream file_1("transforms.txt");
+pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree(resolution);
 
-float dist (pcl::PointXYZ a, pcl::PointXYZ b)
+float dist(pcl::PointXYZ a, pcl::PointXYZ b)
 {
-    return (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y) + (a.z - b.z)*(a.z - b.z);
+	return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z);
 }
 
-int i=0;
-pcl::PointCloud<pcl::PointXYZ>::Ptr unfiltered(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr in_cld(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PCLPointCloud2 pcl_pc2;
-pcl::PointCloud<pcl::PointXYZ>::Ptr edge_cld(new pcl::PointCloud<pcl::PointXYZ>);
+int i = 0;
+pcl::PointCloud<pcl::PointXYZ>::Ptr globalmap(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr localmap(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PCLPointCloud2 in_cloud;
 
-void pc2_to_pcl(const boost::shared_ptr<const sensor_msgs::PointCloud2>& input)
+// void pc2_to_pcl(const boost::shared_ptr<const sensor_msgs::PointCloud2> &input)
+// {
+// 	pcl_conversions::toPCL(*input, in_cloud);
+// 	pcl::fromPCLPointCloud2(in_cloud, *globalmap);
+// }
+
+void extract_local_map(float X, float Y, float Z, float radius)
 {
-  pcl_conversions::toPCL(*input, pcl_pc2);
+	pcl::PointXYZ searchPoint;
+
+	searchPoint.x = X;
+	searchPoint.y = Y;
+	searchPoint.z = Z;
+
+	std::vector<int> pointIdxRadiusSearch;
+	std::vector<float> pointRadiusSquaredDistance;
+
+	// std::cout << "Neighbors within radius search at (" << searchPoint.x
+	// 		  << " " << searchPoint.y
+	// 		  << " " << searchPoint.z
+	// 		  << ") with radius=" << radius << std::endl;
+
+	if (octree.radiusSearch(searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0)
+	{
+		// Generate pointcloud data
+		localmap->width = pointIdxRadiusSearch.size();
+		localmap->height = 1;
+		localmap->points.resize (localmap->width * localmap->height);
+		
+		for (std::size_t i = 0; i < pointIdxRadiusSearch.size(); ++i)
+		{
+			localmap->points[i] =  globalmap->points[pointIdxRadiusSearch[i]];
+		}
+	}
 }
 
-void edge_extract(int K, int thresholdDistance)
-{
-    (*edge_cld).clear();    
-    pcl::fromPCLPointCloud2(pcl_pc2, *unfiltered);
-    
-    pcl::console::print_highlight ("Downsampling...\n");
-    pcl::VoxelGrid<pcl::PointXYZ> grid;
-    grid.setLeafSize (leaf, leaf, leaf);
-    grid.setInputCloud (unfiltered);
-    grid.filter (*unfiltered);
-    
-    pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
-    kdtree.setInputCloud (unfiltered);
-    pcl::PointXYZ searchPoint;
-
-    cout<<endl<<"started"<<endl;
-
-    // K nearest neighbor search
-    std::vector<int> pointIdxNKNSearch(K);
-    std::vector<float> pointNKNSquaredDistance(K);
-
-    std::cout << "K nearest neighbor search at (" << searchPoint.x 
-            << " " << searchPoint.y 
-            << " " << searchPoint.z
-            << ") with K=" << K << std::endl;
-
-    int sz = (*unfiltered).size();
-
-    int edge_pts=0, non_edge_pts = 0;
-
-    for (int iter=0; iter<(*unfiltered).size(); iter++)
-    {
-        searchPoint = unfiltered->points[iter];
-        if ( kdtree.nearestKSearch (searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
-        {
-            pcl::PointXYZ Centroid;
-            Centroid.x = Centroid.y = Centroid.z = 0;
-            int num = 0;
-            float ResDist = 10000;
-            for (std::size_t i = 0; i < pointIdxNKNSearch.size (); ++i)
-            {
-                if (dist(searchPoint, unfiltered->points[ pointIdxNKNSearch[i] ]) > 0.001)
-                {
-                    Centroid.x += unfiltered->points[ pointIdxNKNSearch[i] ].x;
-                    Centroid.y += unfiltered->points[ pointIdxNKNSearch[i] ].y;
-                    Centroid.z += unfiltered->points[ pointIdxNKNSearch[i] ].z;
-                    num++;
-                    float resCheck = dist (searchPoint, unfiltered->points[ pointIdxNKNSearch[i] ]);
-                    if (resCheck < ResDist)
-                    {
-                        ResDist = resCheck;
-                        // cout<<dist(searchPoint, unfiltered->points[ pointIdxNKNSearch[i] ])<<endl;
-                    }
-                }
-                // std::cout << "    "  <<   unfiltered->points[ pointIdxNKNSearch[i] ].x 
-                //         << " " << unfiltered->points[ pointIdxNKNSearch[i] ].y 
-                //         << " " << unfiltered->points[ pointIdxNKNSearch[i] ].z 
-                //         << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
-            }
-            // cout<<                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   endl;
-            if (num>0)
-            {
-                Centroid.x /= num;
-                Centroid.y /= num;
-                Centroid.z /= num;
-            }
-
-            if (dist (Centroid, searchPoint) > thresholdDistance*ResDist)
-            {
-                (*edge_cld).push_back(searchPoint);
-                edge_pts ++;
-            }
-            else
-            {
-                non_edge_pts++;
-            }
-            
-            // cout << iter <<" "<< sz << endl;
-        }
-    }
-    cout << endl << "ended\nedge pts = " << edge_pts << "\nnon edge pts = " << non_edge_pts<<endl;
-}
-
-int main (int argc, char** argv)
+int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "edge_pub");
-    // ros::package::getPath('PKG_NAME')
-    //pcl::io::loadPCDFile<pcl::PointXYZ> ("/media/shreyanshdarshan/New Volume/vision/PCL/XYZ2PCD/build/civil_rotated.pcd", *unfiltered);
-	
-    ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("/velodyne_points", 1000, pc2_to_pcl);
-    ros::Publisher map_pub = n.advertise<sensor_msgs::PointCloud2>("/edge_cloud", 1000);
-    sensor_msgs::PointCloud2 edge_msg;
+	// ros::package::getPath('PKG_NAME')
+	pcl::io::loadPCDFile<pcl::PointXYZ> ("/media/shreyanshdarshan/New Volume/vision/PCL/XYZ2PCD/build/pepsi_down.pcd", *globalmap);
 
-    cout<< "Enter threshold distance" << endl;
-    float thresholdDistance = 1;
-    cin >> thresholdDistance;
-    int K = 20;
-    cout<<"Enter K: "<<endl;
-    cin >> K;
+	ros::NodeHandle n;
+	// ros::Subscriber sub = n.subscribe("/velodyne_points", 1000, pc2_to_pcl);
+	ros::Publisher map_pub = n.advertise<sensor_msgs::PointCloud2>("/local_map", 1000);
+	sensor_msgs::PointCloud2 localmap_msg;
 
-    while (ros::ok())
-    {
-        edge_extract(K, thresholdDistance);
-        pcl::toROSMsg(*edge_cld.get(), edge_msg );
-        edge_msg.header.frame_id = "velodyne";
-        map_pub.publish(edge_msg);
-        //cout<<object_msg;
-	    ros::spinOnce();
-        // break;
-    }
+	octree.setInputCloud(globalmap);
+	octree.addPointsFromInputCloud();
+
+	float x=0, y=0, z=0;
+	while (ros::ok())
+	{
+		x+=0.01;
+		extract_local_map (x, y, z, 20);
+		pcl::toROSMsg(*localmap.get(), localmap_msg);
+		localmap_msg.header.frame_id = "odom";
+		map_pub.publish(localmap_msg);
+		//cout<<object_msg;
+		ros::spinOnce();
+		// break;
+	}
 	return 0;
 }
